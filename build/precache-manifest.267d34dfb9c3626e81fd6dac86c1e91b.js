0cbbd0ca410f1a4f6853ca59a455cd836a18fd01
Dilan Li <dilanli0706@gmail.com>
2020-10-08 17:01:25 -0600

    removed node_modules from gitignore

---
+++ build/precache-manifest.267d34dfb9c3626e81fd6dac86c1e91b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0397eadd20adcd61199087454baa80c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16152dc5f9d64e8f58c7",
+    "url": "/static/css/main.091de721.chunk.css"
+  },
+  {
+    "revision": "f2c6a9f5def77ecd4c82",
+    "url": "/static/js/2.dcf38d9f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dcf38d9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16152dc5f9d64e8f58c7",
+    "url": "/static/js/main.ba532bd0.chunk.js"
+  },
+  {
+    "revision": "c74b6dcb725f329d7bfe",
+    "url": "/static/js/runtime-main.403e0656.js"
+  },
+  {
+    "revision": "d96d7fc7ea319ad050244655e5e3c44e",
+    "url": "/static/media/llamacorn_residents.d96d7fc7.JPG"
+  }
+]);
